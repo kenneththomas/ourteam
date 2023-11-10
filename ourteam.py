e1ad1c2367af69a689ed2f0685d8439b85e95f6d
Kenneth Woo <kennethwoo11@gmail.com>
2023-11-10 17:54:14 -0500

    l

--- ourteam.py
+++ ourteam.py
@@ -78,7 +78,7 @@ def get_management_chain(employee, levels=3):
     return chain
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(host='0.0.0.0', debug=True, port=5002)
 
 '''
 with app.app_context():
