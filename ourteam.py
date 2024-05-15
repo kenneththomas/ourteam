6881dab4ef31a4725660c801774beb3d38309dcf
kenneththomas <kennethwoo11@gmail.com>
2024-05-14 22:02:08 -0400

    prettier

--- ourteam.py
+++ ourteam.py
@@ -253,25 +253,22 @@ def comment():
         db.session.add(comment)
         db.session.commit()
 
-        #award xp to recipient
+        # Award XP to recipient
         recipient_xp = EmployeeXP.query.filter_by(employee_id=to_employee).first()
         if recipient_xp is None:
             recipient_xp = EmployeeXP(employee_id=to_employee, xp=0)
             db.session.add(recipient_xp)
         recipient_xp.xp += xp_actions['receive_comment']
         recipient_xp.level = calculate_level(recipient_xp.xp)
-        #print recipient name and xp
         print(f'recipient: {to_employee} xp: {recipient_xp.xp}')
 
-        #award xp to author
+        # Award XP to author
         author_xp = EmployeeXP.query.filter_by(employee_id=from_employee).first()
         if author_xp is None:
             author_xp = EmployeeXP(employee_id=from_employee, xp=0)
             db.session.add(author_xp)
-
         author_xp.xp += xp_actions['send_comment']
         author_xp.level = calculate_level(author_xp.xp)
-        #print author name and xp
         print(f'author: {from_employee} xp: {author_xp.xp}')
 
         fromname = Employee.query.get(from_employee).name
@@ -282,10 +279,15 @@ def comment():
         db.session.add(action)
         db.session.commit()
 
-        # redirect to the same page so that the user can submit another comment
         flash('Comment submitted successfully')
         return redirect(url_for('comment'))
-    return render_template('test_comment.html')
+
+    comments = db.session.query(Comment).join(Employee, Comment.employee_id == Employee.id).order_by(Comment.id.desc()).limit(5).all()
+    for comment in comments:
+        comment.from_employee = Employee.query.get(comment.author_id)
+        comment.to_employee = Employee.query.get(comment.employee_id)
+    return render_template('test_comment.html', comments=comments)
+
 
 @app.route('/department/<department_name>', methods=['GET'])
 def list_employees_by_department(department_name):
